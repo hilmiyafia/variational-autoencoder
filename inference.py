a0ce8da235e6779d556bbeacd77b6060eacbc108
Hilmi Yafi A <56539775+hilmiyafia@users.noreply.github.com>
2024-09-30 00:23:41 +0700

    Result

--- inference.py
+++ inference.py
@@ -1,21 +1,34 @@
 
 import torch
+import numpy
 import torchvision
-from train import AdversarialAutoencoder
+import matplotlib.pyplot as pyplot
+from dataset import Dataset
+from train import VariationalAutoencoder
 
 
-ROWS = 4
-COLS = 4
-PATH = "lightning_logs/version_0/checkpoints/epoch=63-step=17408.ckpt"
+ROWS = 8
+COLS = 8
+BATCH = 16
+PATH = "lightning_logs/version_0/checkpoints/epoch=999-step=136000.ckpt"
 
 
 if __name__ == "__main__":
-    model = AdversarialAutoencoder.load_from_checkpoint(PATH).eval()
+    model = VariationalAutoencoder.load_from_checkpoint(PATH).eval()
+    dataset = Dataset("faces")
     with torch.no_grad():
-        random = torch.randn(ROWS*COLS, 256)
+        codes = []
+        for i in range(0, len(dataset), BATCH):
+            j = min(i+BATCH, len(dataset))
+            batch = torch.stack([dataset[k] for k in range(i, j)])
+            batch = batch.type_as(model.autoencoder.encoder[0].layers[0].weight)
+            codes.append(model.autoencoder.encoder(batch).cpu()[:, :256])
+        codes = torch.concat(codes)
+        mean = codes.mean(0, keepdim=True)
+        std = codes.std(0, keepdim=True)
+        random = mean + std*torch.randn(ROWS*COLS, 256)
         random = random.type_as(model.autoencoder.decoder[-1].layers[0].weight)
         random = model.autoencoder.decoder(random) * 255
         random = torchvision.utils.make_grid(random, nrow=ROWS, padding=0)
         random = random.clamp(min=0, max=255).to(torch.uint8).cpu()
         torchvision.io.write_jpeg(random, "result.jpg")
-
